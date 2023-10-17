67dbb09456c6ec55b03d9d30cd26daf9b24b8349
UstinovAlexey <ustinov@udm.ru>
2023-10-18 00:12:51 +0400

    main test 5

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ CLIENT_ID = "stm32_baxi"
 SUBSCRIBE_TOPIC = b"baxi/GitOTA"
 PUBLISH_TOPIC = b"baxi/GitOTA"
 gitver="V1"
+needGitOTAupdate=False
 
 def sub_cb(topic, msg):
+    global needGitOTAupdate
     print((topic, msg))
     if (topic==b"baxi/GitOTA") and (msg==b"PullGit"):
-        ugit.pull_all()
+        needGitOTAupdate=True
+
 
 mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, port=1883,user="user",password="89127634678",keepalive=60)
 mqttClient.set_callback(sub_cb)
@@ -24,9 +27,17 @@ for i in range (10):
     time.sleep(1)
     # Non-blocking wait for message
     mqttClient.check_msg()
-    
-    mqttClient.publish(PUBLISH_TOPIC, "Waiting OTA Cmd {}".format(i))
+            
+    if needGitOTAupdate==True:
+        mqttClient.publish(PUBLISH_TOPIC, "OTA: Now Git pulled...")
+        print("ww")
 
+    else:
+        mqttClient.publish(PUBLISH_TOPIC, "Waiting OTA Cmd {}".format(i))
+        print("w")
+
+if needGitOTAupdate==True:
+        ugit.pull_all()
     
 mqttClient.publish(PUBLISH_TOPIC, "Run application. GitVer={}".format(gitver))
 
